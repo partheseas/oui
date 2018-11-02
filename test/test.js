6f0eddef3468ae500fc255c64e3bf314756a8494
Kayla Washburn <partheseas@gmail.com>
2018-11-02 08:58:18 -0600

    Simplify adding test cases, and document variables a little

--- test/test.js
+++ test/test.js
@@ -1,8 +1,22 @@
+let styles = [ 'background-color: #000000; color: white',
+               'background-color: #e78686',
+               'background-color: #e7c686',
+               'background-color: #dfe786',
+               'background-color: #86e78d',
+               'background-color: #86e7e4',
+               'background-color: #9186e7',
+               'background-color: #6657c6',
+               'background-color: #1400ff',
+               'background-color: #e786d7' ]
+
 window.addEventListener( 'DOMContentLoaded', () => {
   let source = document.querySelector( '#test-source' )
   let fills = Array.from( document.querySelectorAll( '.test-fill' ) )
 
-  fills.forEach( container => {
+  styles.forEach( style => {
+    let container = document.createElement( 'section' )
     container.innerHTML = source.innerHTML
+    container.style = style
+    document.body.appendChild( container )
   })
 })
